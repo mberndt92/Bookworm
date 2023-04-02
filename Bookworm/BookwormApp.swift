9b4bd53f19323b9628b22e1a1a7691c2cb33c0e8
mberndt92 <maximilian_berndt@icloud.com>
2023-04-02 09:34:23 +0900

    Initial Commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Maximilian Berndt on 2023/04/02.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
